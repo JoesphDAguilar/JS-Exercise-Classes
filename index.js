c4ee0ba34a78b57d616fa27edf35d2beb88462ab
JoesphAguilar <josephjd93@gmail.com>
2022-02-17 15:00:18 -0800

    Completed creating Instance of Car and passed all test for Task 2

--- index.js
+++ index.js
@@ -75,7 +75,26 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    return this.tank += gallons;
+  }
+  drive(distance) {
+    const milesAvailable = this.tank * this.milesPerGallon;
+    if(distance <= milesAvailable){
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance / this.milesPerGallon);
+    }else {
+      this.odometer = this.odometer + milesAvailable;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }
 }
 
 /*
